0e55f1ed6709402d88f217022efd86b1e84b7acd
Jheorgenes Warlley <jheorgenes@gmail.com>
2024-11-18 18:15:21 -0300

    -- Organizando os Menus e Sub-Menus do painel

--- app/Providers/NovaServiceProvider.php
+++ app/Providers/NovaServiceProvider.php
@@ -2,7 +2,22 @@
 
 namespace App\Providers;
 
+use App\Nova\Brand;
+use App\Nova\Client;
+use App\Nova\Dashboards\Main;
+use App\Nova\Installer;
+use App\Nova\Operator;
+use App\Nova\Permission;
+use App\Nova\Regional;
+use App\Nova\ServiceOrder;
+use App\Nova\SimCard;
+use App\Nova\TableService;
+use App\Nova\Tracker;
+use App\Nova\User;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Gate;
+use Laravel\Nova\Menu\MenuItem;
+use Laravel\Nova\Menu\MenuSection;
 use Laravel\Nova\Nova;
 use Laravel\Nova\NovaApplicationServiceProvider;
 
@@ -19,6 +34,30 @@ class NovaServiceProvider extends NovaApplicationServiceProvider
 
         // Exibindo o caminho da navegação
         Nova::withBreadcrumbs();
+
+        // Definindo a organização de navegação com menus
+        Nova::mainMenu(function (Request $request) {
+            return [
+                MenuSection::dashboard(Main::class)->icon('chart-bar'),
+
+                MenuSection::make('Usuario', [
+                    MenuItem::resource(User::class),
+                    MenuItem::resource(Permission::class),
+                    MenuItem::resource(Client::class),
+                ])->icon('user')->collapsable(),
+
+                MenuSection::make('Rastreamento', [
+                    MenuItem::resource(Brand::class),
+                    MenuItem::resource(Operator::class),
+                    MenuItem::resource(SimCard::class),
+                    MenuItem::resource(Tracker::class),
+                    MenuItem::resource(Installer::class),
+                    MenuItem::resource(Regional::class),
+                    MenuItem::resource(TableService::class),
+                    MenuItem::resource(ServiceOrder::class),
+                ])->icon('chip')->collapsable(),
+            ];
+        });
     }
 
     /**
